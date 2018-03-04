489af38c760bae7f8be16452568f158e94a5e6a2
pythonjia <960359972@qq.com>
2018-03-04 18:46:12 +0800

    Initial commit

---
+++ Renren/spiders/renren.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class RenrenSpider(scrapy.Spider):
+    name = 'renren'
+    allowed_domains = ['renren.com']
+    start_urls = ['http://www.renren.com/PLogin.do']
+
+    def start_requests(self):
+        
+
+    def parse(self, response):
+        pass
