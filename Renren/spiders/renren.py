baddf44b0360ce8580e8d637659318cc25364ff6
pythonjia <960359972@qq.com>
2018-03-04 19:04:59 +0800

    一个简单的post表单请求完成模拟登陆

--- Renren/spiders/renren.py
+++ Renren/spiders/renren.py
@@ -6,9 +6,16 @@ class RenrenSpider(scrapy.Spider):
     name = 'renren'
     allowed_domains = ['renren.com']
     start_urls = ['http://www.renren.com/PLogin.do']
-
+    # 重写start_requests方法:因start_requests方法默认是get请求，此时需要重写将其改成post请求
     def start_requests(self):
-        
+        # 构造post请求参数
+        data = {
+            'email':'214578765@qq.com',
+            'password':'MYSELF93542'
+        }
+        # 发送请求
+        yield scrapy.FormRequest(url=self.start_urls[0],formdata=data)
 
     def parse(self, response):
-        pass
+        with open('renren.html','wb') as f:
+            f.write(response.body)
